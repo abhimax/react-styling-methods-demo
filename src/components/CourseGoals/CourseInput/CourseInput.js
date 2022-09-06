d209180bde26e7628d4b3354f7dfc0535ce2c21e
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-06 09:09:14 +0530

    feat: use styled-component dynamic props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,19 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+const FormControl = styled.div`
+  margin: 0.5rem 0;
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    background: #fad0ec;
+    border-color: red;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
+
+const DynamicTile = styled.div`
+width: 140px;
+height: 140px;
+background-color: ${ props =>  props.invalid ? 'red' : 'green'};
+-webkit-align-content: center;
+-ms-flex-line-pack: center;
+padding: 10px;
+box-sizing: border-box;
+margin-top: 20px;
+justify-content: center;
+display: flex;
+flex-direction: column;
+align-items: center;
+& h3{
+  color: white;
+}
+`;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    if( enteredValue.trim().length === 0){
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
       console.log("empty!");
@@ -22,13 +74,22 @@ const CourseInput = props => {
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
+    <>
+      <form onSubmit={formSubmitHandler}>
+        <FormControl className={`${!isValid ? "invalid" : ""}`}>
+          <label style={{ color: !isValid ? "red" : "black" }}>
+            Course Goal
+          </label>
+          <input type="text" onChange={goalInputChangeHandler} />
+        </FormControl>
+        <Button type="submit">Add Goal</Button>
+      </form>
+      <DynamicTile invalid={!isValid}>
+        <div className="indicator"></div>
+        <h3>Test Styled Component</h3>
+        <p>Dynamic props</p>
+      </DynamicTile>
+    </>
   );
 };
 
